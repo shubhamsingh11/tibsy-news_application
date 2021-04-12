89c0248d23819ac1eaaaffbc21d0928c1826f25b
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-04-13 02:17:24 +0530

    Added News Cards and their styling

--- src/components/newsCards/NewsCards.js
+++ src/components/newsCards/NewsCards.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { Grid , Grow , Typography } from '@material-ui/core';
 import NewsCard from '../NewsCard/NewsCard'
 
+import useStyles from '../NewsCard/styles';
+
 const NewsCards = ({ articles }) => {
+    const classes = useStyles();
+
     return (
         <Grow in>
-            {articles.map((articles,i) => (
-                <NewsCard />
-            ))}    
+           <Grid classNmae={classes.container} container alignItems="stretch" spacing ={3}>
+                
+                {articles.map((article,i) => (
+                    <Grid key={i} item xs={12} sm ={6} md={4} lg={3} style={{display : 'flex'}}>
+                        <NewsCard article={article} i={i}/>
+                    </Grid>
+                ))}
+                
+             </Grid> 
+                
         </Grow>
     );
 }
