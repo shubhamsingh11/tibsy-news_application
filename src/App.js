4da66760ab61433e3fceba982b1e8781bb3a5339
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-04-23 04:49:38 +0530

    Finishing up the app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,58 @@
-import React , {useState , useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@material-ui/core';
+import wordsToNumbers from 'words-to-numbers';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
-import NewsCards from './components/newsCards/NewsCards'
-
-const alanKey = 'ec1bcfa629c0ef9a25fe64f72fcffd2c2e956eca572e1d8b807a3e2338fdd0dc/stage';
+import logo from './images/logo78.jpg';
+import NewsCards from './components/newsCards/NewsCards';
+import useStyles from './styles';
 
 const App = () => {
-    const [newsArticles , setNewsArticles ] = useState([]);
+  const [activeArticle, setActiveArticle] = useState(0);
+  const [newsArticles, setNewsArticles] = useState([]);
 
-    useEffect(() => {
-        alanBtn({
-            key : alanKey,
-            onCommand : ({command , articles}) => {
-                if(command === 'newHeadlines'){
-                    setNewsArticles(articles);
-                }
-            }
+  const classes = useStyles();
+
+  useEffect(() => {
+    alanBtn({
+      key: 'ec1bcfa629c0ef9a25fe64f72fcffd2c2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if (command === 'highlight') {
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if (command === 'open') {
+          const parsedNumber = number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1];
+
+          if (parsedNumber > articles.length) {
+            alanBtn().playText('Please try that again...');
+          } else if (article) {
+            window.open(article.url, '_blank');
+            alanBtn().playText('Opening...');
+          } else {
+            alanBtn().playText('Please try that again...');
+          }
         }
-        )
-    }, []) 
-    //The second [] is an dependency array which decides how many times useEffect will be called
+      },
+    });
+  }, []);
 
-    return(
-        <div>
-            <h1>News Application Powered by Alan AI</h1>
-            <NewsCards articles={newsArticles} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        {newsArticles.length ? (
+          <div className={classes.infoContainer}>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Open article number [4]</Typography></div>
+            <div className={classes.card}><Typography variant="h5" component="h2">Try saying: <br /><br />Go back</Typography></div>
+          </div>
+        ) : null}
+        <img src={logo} className={classes.alanLogo} alt="logo" />
+      </div>
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+    </div>
+  );
+};
 
 export default App;
